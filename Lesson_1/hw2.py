e4d3a636ac1a846d6ada23bb3591de4a446372e5
Tsyhankova <tt1846641@gmail.com>
2020-12-22 01:09:54 +0100

    hw3

--- Lesson_1/hw2.py
+++ Lesson_1/hw2.py
@@ -3,8 +3,8 @@ second_coef = float(input ('enter the second coefficient,\n b = '))
 third_coef = float(input ('enter the third coefficient,\n c = '))
 
 D = second_coef**2 - 4*first_coef*third_coef
-x1 = format((-second_coef + D**0.5)/2*first_coef, '.2f')
-x2 = format((-second_coef - D**0.5)/2*first_coef, '.2f')
+x1 = format((-second_coef + D**0.5)/(2*first_coef), '.2f')
+x2 = format((-second_coef - D**0.5)/(2*first_coef), '.2f')
 
 print(f'x1 = {x1}; x2 = {x2}')
 
