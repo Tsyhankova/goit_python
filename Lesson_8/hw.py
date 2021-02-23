8ab7e0971f207223fef114f82c8ac8dbe52748a7
Tsyhankova <tt1846641@gmail.com>
2021-02-24 00:34:13 +0100

    update

--- Lesson_8/hw.py
+++ Lesson_8/hw.py
@@ -1,54 +1,65 @@
 from datetime import datetime, timedelta
+import random
+import string
 
 
-users = [{'name': 'Any', 'birthday': datetime(year=2000, month=3, day=1).date()},
-          {'name': 'An', 'birthday': datetime(year=2011, month=3, day=2).date()},
-          {'name': 'Go', 'birthday': datetime(year=2021, month=3, day=3).date()},
-          {'name': 'Igor', 'birthday': datetime(year=2001, month=2, day=27).date()},
-          {'name': 'Daniel', 'birthday': datetime(year=2021, month=2, day=28).date()},
-          {'name': 'Ja', 'birthday': datetime(year=2021, month=3, day=1).date()},
-          {'name': 'Kiril', 'birthday': datetime(year=2021, month=3, day=2).date()},
-          {'name': 'Ty', 'birthday': datetime(year=2021, month=3, day=2).date()},
-          {'name': 'My', 'birthday': datetime(year=2021, month=3, day=1).date()}]
+def congratulate(users):
 
+    d = datetime.now()
 
+    monday = []
+    tuesday = []
+    wednesday = []
+    thursday = []
+    friday = []
+    if d.weekday() == 5:
+        for i in range (7):
+            next_week = (d + timedelta(days = i)).date()
+    else:
+        for i in range (7):
+            next_week = (d + timedelta(days = i + (5 - d.weekday()))).date()
 
-
-
-
-d = datetime.now()
-
-monday = []
-tuesday = []
-wednesday = []
-thursday = []
-friday = []
-   
-
-list_for_week = []
-
-if d.weekday() == 1:
-    s = (datetime.now() + timedelta(days = 4)).date()
-    for i in range (7):
-        next_week = (datetime.now() + timedelta(days = 4+i)).date()
-        for user in users:
-            if (next_week.month == user["birthday"].month and next_week.day == user["birthday"].day):     
-                if next_week.weekday() == 1:
-                    tuesday.append(user['name'])
-                elif next_week.weekday() == 2:
-                    wednesday.append(user['name'])
-                elif next_week.weekday() == 3:
-                    thursday.append(user['name'])
-                elif next_week.weekday() == 4:
-                    friday.append(user['name'])
-                else:
-                    monday.append(user['name'])
+            for user in users:
+                if (next_week.month == user["birthday"].month and next_week.day == user["birthday"].day):
+                    
+                    if next_week.weekday() == 1:
+                        tuesday.append(user['name'])
+                    elif next_week.weekday() == 2:
+                        wednesday.append(user['name'])
+                    elif next_week.weekday() == 3:
+                        thursday.append(user['name'])
+                    elif next_week.weekday() == 4:
+                        friday.append(user['name'])
+                    else:
+                        monday.append(user['name'])
     
-                #print(user['name'])
-        #print(next_week)
-print(', '.join(monday))
+        print(f"Monday: {', '.join(monday)}\nTuesday: {', '.join(tuesday)}\nWednesday: {', '.join(wednesday)}\nThursday: {', '.join(thursday)}\nFriday: {', '.join(friday)}\n")
+    return 'done'
+
+users = []
+def random_users():
+    start_date = datetime(year=1985, month=3, day=20).date()
+    end_date = datetime(year=1985, month=2, day=20).date()
+    interval_delta = start_date - end_date
     
-    
+    for _ in range(30): 
+        days_delta = random.randint(0, interval_delta.days)
+        birthday_value = end_date + timedelta(days = days_delta)
+        name_value = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        dict_user = {'name':name_value, 'birthday': birthday_value}
+        users.append(dict_user)
+    return users
+
+def main():
+    random_users()
+    congratulate(users)
+
+
+
+if __name__ == "__main__":
+    main()
+        
+
 
         
         
